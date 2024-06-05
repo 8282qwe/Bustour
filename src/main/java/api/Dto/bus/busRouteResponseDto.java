1caa3729b0c6b3d071cf8a591b4f994c6eb13733
jangwoolee <8282qwe@gmail.com>
2024-06-05 10:43:25 +0900

    swagger 수정

--- src/main/java/api/Dto/bus/busRouteResponseDto.java
+++ src/main/java/api/Dto/bus/busRouteResponseDto.java
@@ -1,5 +1,6 @@
 package api.Dto.bus;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Data
@@ -7,11 +8,18 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class busRouteResponseDto {
+    @ApiModelProperty(example = "종점")
     private String endnodenm;
+    @ApiModelProperty(example = "막차 시간")
     private String endvehicletime;
+    @ApiModelProperty(example = "노선 ID")
     private String routeid;
+    @ApiModelProperty(example = "노선 번호")
     private String routeno;
+    @ApiModelProperty(example = "버스 종류")
     private String routetp;
+    @ApiModelProperty(example = "기점")
     private String startnodenm;
+    @ApiModelProperty(example = "첫차 시간")
     private String startvehicletime;
 }
--- src/main/java/api/Dto/keyWord/favoriteRequestDto.java
+++ src/main/java/api/Dto/keyWord/favoriteRequestDto.java
@@ -1,5 +1,6 @@
 package api.Dto.keyWord;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +13,16 @@ import org.apache.ibatis.type.Alias;
 @AllArgsConstructor
 @Alias("favoriteRequestDto")
 public class favoriteRequestDto {
+    @ApiModelProperty(example = "유저의 ID")
     private String id;
+    @ApiModelProperty(example = "가게 이름")
     private String title;
+    @ApiModelProperty(example = "가게 링크")
     private String link;
+    @ApiModelProperty(example = "가게 카테고리")
     private String category;
+    @ApiModelProperty(example = "가게 주소")
     private String address;
+    @ApiModelProperty(example = "가게 도로명 주소")
     private String roadaddress;
 }
